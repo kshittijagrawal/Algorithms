f805b377dd98517ca6502cbb85ecc231ae606d39
kshitij <agrawal.kshitij08@gmail.com>
2021-02-02 23:50:20 +0530

    Add Euclid's Algo

---
+++ Euclidean/lcm.py
@@ -0,0 +1,10 @@
+def final_gcd(a, b):
+    if b == 0:
+        return a
+    aa = a % b
+    return final_gcd(b, aa)
+
+numbers = input()
+a, b = int(numbers.split(" ")[0]), int(numbers.split(" ")[1])
+res = final_gcd(a, b)
+print((a * b)//res)
