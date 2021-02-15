d708164c4939d3cc33ec6467f61c7f9bee5b310a
kshitij <agrawal.kshitij08@gmail.com>
2021-02-15 20:48:39 +0530

    Add comments to the main files

--- Euclidean/lcm.py
+++ Euclidean/lcm.py
@@ -1,10 +1,12 @@
+# funtion computing the gcd
 def final_gcd(a, b):
     if b == 0:
         return a
     aa = a % b
     return final_gcd(b, aa)
 
+# the main driver code
 numbers = input()
 a, b = int(numbers.split(" ")[0]), int(numbers.split(" ")[1])
 res = final_gcd(a, b)
-print((a * b)//res)
+print((a * b)//res) # since the lcm of 2 numbers equals floor of (their product)/(gcd)
