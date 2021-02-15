5752fd12d1d49cb21e81353fba7ed819ae4986b2
kshitij <agrawal.kshitij08@gmail.com>
2021-02-15 20:47:02 +0530

    Add comments to the main file

--- Karatsuba/poly-multi.py
+++ Karatsuba/poly-multi.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# n is the number of terms in the quadratic eq
+# a and b are the particular terms of our final result which we aim to find. During the start, it is 0 each
 def poly_mult_dc_naive(A, B, n, a, b):
   C = np.zeros(2*n - 1, dtype=int)
   if n == 1:
@@ -14,3 +16,8 @@ def poly_mult_dc_naive(A, B, n, a, b):
 
 if __name__ == '__main__':
     print(poly_mult_dc_naive([0,3,2,5], [0,5,1,2], 4, 0, 0))
+
+# here A(x) = 0x^3 + 3x^2 + 2x^1 + 5 ---> 3x^2 + 2x^1 + 5
+# and B(x) = 0x^3 + 5x^2 + x^1 + 2 ---> 5x^2 + x^1 + 2
+# which results in [0  0 15 13 33  9 10]
+# i.e 0x^6 + 0x^5 + 15x^4 + 13x^3 + 33x^2 + 9x + 10 ---> 15x^4 + 13x^3 + 33x^2 + 9x + 10
